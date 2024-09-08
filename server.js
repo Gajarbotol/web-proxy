b1724408598f18c78e82b9b38ab5818dfc1f5f06
Gajarbotol <123944033+Gajarbotol@users.noreply.github.com>
2024-09-08 15:15:45 +0600

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,60 @@
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
-const path = require('path');
+const axios = require('axios');
+const cheerio = require('cheerio'); // To parse and modify HTML
 
 const app = express();
 
-// Serve the HTML page that contains the iframe
+// Serve the HTML page containing the iframe
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(__dirname + '/index.html');
 });
 
-// Proxy requests to the target website
-app.use('/proxy', createProxyMiddleware({
-    target: 'https://en.pornohd.blue/',
-    changeOrigin: true,
-    pathRewrite: {
-        '^/proxy': '', // remove /proxy from the URL
-    },
-}));
+// Middleware to fetch and modify the content from the target website
+app.get('/proxy*', async (req, res) => {
+    try {
+        const targetUrl = 'https://en.pornohd.blue/' + req.url.replace('/proxy', '');
+        const response = await axios.get(targetUrl);
 
+        // If the response is HTML, rewrite links
+        if (response.headers['content-type'].includes('text/html')) {
+            const $ = cheerio.load(response.data);
+
+            // Rewrite all links to pass through the proxy
+            $('a').each((_, el) => {
+                const href = $(el).attr('href');
+                if (href && href.startsWith('/')) {
+                    $(el).attr('href', '/proxy' + href);
+                } else if (href && href.startsWith('http')) {
+                    $(el).attr('href', '/proxy?url=' + href);
+                }
+            });
+
+            // Rewrite all other resources (like images, scripts, etc.)
+            $('img, script, link').each((_, el) => {
+                const src = $(el).attr('src');
+                const href = $(el).attr('href');
+
+                if (src && src.startsWith('/')) {
+                    $(el).attr('src', '/proxy' + src);
+                }
+                if (href && href.startsWith('/')) {
+                    $(el).attr('href', '/proxy' + href);
+                }
+            });
+
+            res.send($.html());
+        } else {
+            // For non-HTML resources, just send them directly
+            res.set(response.headers);
+            res.send(response.data);
+        }
+    } catch (err) {
+        res.status(500).send('Error fetching content');
+    }
+});
+
+// Start the server
 app.listen(3000, () => {
-    console.log('Proxy server is running on http://localhost:3000');
+    console.log('Server running on http://localhost:3000');
 });
