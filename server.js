84d301a3e2005516b2134cb82cde4f69e8ed7562
Gajarbotol <123944033+Gajarbotol@users.noreply.github.com>
2024-09-08 14:24:25 +0600

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 // Proxy requests to the target URL
 app.use('/', createProxyMiddleware({
-    target: 'https://xnxx.com', // target website
+    target: 'en.oxtube.tv', // target website
     changeOrigin: true,
     onProxyReq: function (proxyReq, req, res) {
         // Modify headers, etc. if needed
